a9ba36eeb569f08a05dcff0e35d6fcb6ae3693a5
himanshu <Himanshu.jadoun2024@glbajajgroup.org>
2026-06-12 22:41:06 +0530

    dsa

--- src/com/JAVA_Programing/javabsic/Question_on_2D_array.java
+++ src/com/JAVA_Programing/javabsic/Question_on_2D_array.java
@@ -1,4 +1,29 @@
 package com.JAVA_Programing.javabsic;
 
+import java.util.Scanner;
+
 public class Question_on_2D_array {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] array=new int[n][m];
+        System.out.println("enter the elements of the array");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array[i][j]=sc.nextInt();
+
+            }
+
+        }
+        System.out.println("transpose of the matrix");{
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <m; j++) {
+                    System.out.print(array[j][i]+" ");
+                }
+
+            }
+        }
+    }
 }
